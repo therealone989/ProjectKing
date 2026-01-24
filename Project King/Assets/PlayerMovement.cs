ca462ad232a476542ca297658c8a88de16fc234e
MarkiBoy69 <markriemann.mr@gmail.com>
2026-01-24 20:22:23 +0100

    Movement fixed

--- Project King/Assets/PlayerMovement.cs
+++ Project King/Assets/PlayerMovement.cs
@@ -1,20 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-
     Vector2 movementVector;
     public float moveSpeed = 8f;
+    Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
 
     public void InputPlayer(InputAction.CallbackContext _context)
     {
         movementVector = _context.ReadValue<Vector2>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementVector.x, 0, movementVector.y);
-        movement.Normalize();
-        transform.Translate(moveSpeed * movement * Time.deltaTime);
+        Vector3 dir = new Vector3(movementVector.x, 0, movementVector.y);
+        if(dir.sqrMagnitude > 1)
+        {
+            dir.Normalize();
+        }
+        Vector3 targetpos = rb.position + dir * moveSpeed * Time.deltaTime;
+        rb.MovePosition(targetpos);
     }
 }
