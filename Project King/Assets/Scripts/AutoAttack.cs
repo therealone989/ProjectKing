6ea14048a8149e069f4979cbb3fb811b674f4e55
therealone989 <mrfabigm@gmail.com>
2026-01-25 12:26:08 +0100

    Autoattack
    Autoattack script added
    Projectile Script added
    Enemy Script added

---
+++ Project King/Assets/Scripts/AutoAttack.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAttack : MonoBehaviour
+{
+
+    public Transform shootPoint;
+    public GameObject arrowPrefab;
+    public int damage = 5;
+    public int cooldownMs = 500;
+
+    List<Enemy> enemiesInRange = new();
+    int lastAttackTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemiesInRange.Count == 0) return;
+        if (Time.time * 1000 - lastAttackTime < cooldownMs) return;
+
+        Enemy target = enemiesInRange[0];
+        Shoot(target);
+        lastAttackTime = (int)(Time.time * 1000);
+
+    }
+
+    void Shoot(Enemy target)
+    {
+        GameObject proj = Instantiate(arrowPrefab, shootPoint.position, Quaternion.identity);
+
+        // Pfeil wird Initialisiert mit unsere Damage werte und unseren Target - > Projectile Script lässt pfeil zum gegner fliegen
+        proj.GetComponent<Projectile>().Init(target, damage);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy") && 
+            other.TryGetComponent(out Enemy e))
+        {
+            enemiesInRange.Add(e);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy") &&
+            other.TryGetComponent(out Enemy e))
+        {
+            enemiesInRange.Remove(e);
+        }
+    }
+}
---
+++ Project King/Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Enemy target;
+    int damage;
+    public float speed = 10f;
+
+    public void Init(Enemy e, int dmg)
+    {
+        target = e;
+        damage = dmg;
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.transform.position - transform.position;
+        transform.position += dir.normalized * speed * Time.deltaTime;
+
+        if (dir.magnitude < 0.3f)
+        {
+            // TAKEDAMAGE ENEMY SCRIPT
+            Destroy(gameObject);
+        }
+    }
+}
