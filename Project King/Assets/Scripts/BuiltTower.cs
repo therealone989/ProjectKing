acac6693742e1be658ccff429b8838539e83c819
MarkiBoy69 <markriemann.mr@gmail.com>
2026-02-11 16:35:37 +0100

    Towerplacement erweitert

--- Project King/Assets/Scripts/BuiltTower.cs
+++ Project King/Assets/Scripts/BuiltTower.cs
@@ -1,75 +1,92 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BuiltTower : MonoBehaviour
 {
+    [Header("Colors")] 
+    Color activeColor = new Color(1.0f,1.0f, 1.0f, 1.0f);
+    Color inactiveColor = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+    Color hiddenColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+    [Header("UI References")]
     public Button button;
-    Color newColor;
     public GameObject buildPanel;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Tower References")]
+    private GameObject currentBuildSpot;
+
+    [Header("Tower Prefabs")]
+    public GameObject canonPrefab;
+    public GameObject archerPrefab;
+    public GameObject wizardPrefab;
+    public GameObject troopsPrefab;
+    public enum TowerType{Cannon,Archer,Wizard,Troops}
     void Start()
     {
-        newColor = new Color(1.0f,1.0f, 1.0f, 0.5f); 
-        button.enabled = false;
-        button.image.color = newColor;
+        SetButtonState(false, inactiveColor);
         buildPanel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Buildspot")
+        if (other.gameObject.tag == "Buildspot")
         {
-            button.enabled = true;
-            newColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            button.image.color = newColor;
+            SetButtonState(true, activeColor);
+            currentBuildSpot = other.gameObject;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Buildspot")
         {
-            button.enabled = false;
-            newColor = new Color(1.0f, 1.0f, 1.0f, 0.5f);
-            button.image.color = newColor;
+            SetButtonState(false, inactiveColor);
         }
-    }
-    public void PlaceCanon()
-    {
-        Debug.Log("Place Canon!");
-        ExitMenu();
-    }
-    public void PlaceArcher()
-    {
-        Debug.Log("Place Archer!");
-        ExitMenu();
-    }
-    public void PlaceWizard()
-    {
-        Debug.Log("Place Wizard!");
-        ExitMenu();
-    }
-    public void PlaceTroops()
-    {
-        Debug.Log("Place Troops!");
-        ExitMenu();
     }
     public void ActivateBuildPanel()
     {
         buildPanel.SetActive(true);
-        button.enabled = false;
-        newColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
-        button.image.color = newColor;
+        SetButtonState(false, hiddenColor);
+    }
+    private void SetButtonState(bool isEnabled, Color color)
+    {
+        button.interactable = isEnabled;
+        button.image.color = color;
+    }
+    public void PlaceTower(int towerIndex)
+    {
+        TowerType selectedTower = (TowerType)towerIndex;
+        Debug.Log("Gewaehlter Tower: " + selectedTower);
+        switch (selectedTower)
+        {
+            case TowerType.Cannon:
+                Debug.Log("Kanone wird gebaut!");
+                SpawnTower(canonPrefab);
+                break;
+            case TowerType.Archer:
+                Debug.Log("Archer wird gebaut!");
+                SpawnTower(archerPrefab);
+                break;
+            case TowerType.Wizard:
+                SpawnTower(wizardPrefab);
+                Debug.Log("Wizard wird gebaut!");
+                break;
+            case TowerType.Troops:
+                SpawnTower(troopsPrefab);
+                Debug.Log("Troops wird gebaut!");
+                break;
+            default:
+                break;
+        }
+        ExitMenu();
+    }
+    public void SpawnTower(GameObject towerPrefab)
+    {
+        Instantiate(towerPrefab,currentBuildSpot.transform.position, currentBuildSpot.transform.rotation);
     }
     public void ExitMenu()
     {
         buildPanel.SetActive(false);
-        button.enabled = true;
-        newColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-        button.image.color = newColor;
+        SetButtonState(true, activeColor);
     }
 }
