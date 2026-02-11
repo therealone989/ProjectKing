4df2ceaca392665495a7071d50254fb81af93cc1
MarkiBoy69 <markriemann.mr@gmail.com>
2026-02-11 23:40:04 +0100

    Coin mechanik mit script geändert

---
+++ Project King/Assets/Scripts/CoinCollector.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    public int collectionRange;
+    public LayerMask coinMask;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 10 == 0)
+        {
+            CollectCoin();
+        }
+    }
+public void CollectCoin()
+{
+    // Wallet nur 1x holen
+    PlayerWallet wallet = gameObject.GetComponentInParent<PlayerWallet>();
+    if (wallet == null)
+    {
+        Debug.LogError("Keine Playerwallet!");
+        return;
+    }
+
+    Collider[] hitColliders = Physics.OverlapSphere(transform.position, collectionRange, coinMask);
+
+    foreach (var hitCollider in hitColliders)
+    {
+        CoinDrop coin = hitCollider.GetComponentInParent<CoinDrop>();
+        if (coin == null) 
+            continue;
+
+        // Schon unterwegs? Dann nicht nochmal zaehlen/triggern
+        if (coin.flyingToPlayer)
+            continue;
+
+        coin.flyingToPlayer = true;
+        coin.LocatePlayer(gameObject);
+
+        wallet.Add(1);
+    }
+}
+
+}
--- Project King/Assets/Scripts/CoinDrop.cs
+++ Project King/Assets/Scripts/CoinDrop.cs
@@ -2,29 +2,19 @@ using UnityEngine;
 
 public class CoinDrop : MonoBehaviour
 {
-
-    private Rigidbody rb;
     private ObjectPool pool;
 
     private Vector3 startPos;
     private Vector3 targetOffset;
     private float timer;
 
-    bool flyingToPlayer = false;
-    Transform player;
-
-    private bool isPickedUp = false;
-
+    public bool flyingToPlayer = false;
+    GameObject player;
     private void OnEnable()
     {
         flyingToPlayer = false;
         player = null;
         timer = 0f;
-    }
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
     }
 
     public void Init(ObjectPool pool)
@@ -37,27 +27,24 @@ public class CoinDrop : MonoBehaviour
         Vector2 rand = Random.insideUnitCircle * 4.5f;
         targetOffset = new Vector3(rand.x, 0.2f, rand.y);
     }
-
     void FixedUpdate()
     {
         if (flyingToPlayer)
         {
-            Vector3 targetPos = player.position + Vector3.up;
+            Vector3 targetPos = player.transform.position + Vector3.up;
 
             transform.position = Vector3.MoveTowards(
                 transform.position,
                 targetPos,
                 Time.fixedDeltaTime * 10f
             );
-            if (Vector3.Distance(transform.position, player.position) < 1.2f)
+            if (Vector3.Distance(transform.position, player.transform.position) < 1.2f)
             {
-                isPickedUp = false;
                 pool.ReturnObject(gameObject);
             }
 
             return;
         }
-
         // sanfte Wurfbewegung
         timer += Time.fixedDeltaTime;
         float t = Mathf.Clamp01(timer / 0.4f);
@@ -68,16 +55,8 @@ public class CoinDrop : MonoBehaviour
         // Rotation
         transform.Rotate(Vector3.up, 100f * Time.fixedDeltaTime);
     }
-
-    private void OnTriggerEnter(Collider other)
+    public void LocatePlayer(GameObject go)
     {
-        if (isPickedUp) return;
-        if (!other.CompareTag("Player")) return;
-        isPickedUp = true;
-        PlayerWallet wallet = other.GetComponent<PlayerWallet>();
-        wallet.Add(1);
-        player = other.transform;
-        flyingToPlayer = true;
+        player = go;
     }
-
 }
