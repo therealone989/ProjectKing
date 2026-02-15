f074bdbc82ea560b887506d77afad8629936b8b2
therealone989 <mrfabigm@gmail.com>
2026-02-15 23:31:35 +0100

    Scene angepasst

--- Project King/Assets/Scripts/PlayerWallet.cs
+++ Project King/Assets/Scripts/PlayerWallet.cs
@@ -1,12 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerWallet : MonoBehaviour
 {
+    [SerializeField] TMP_Text coinsText;
     public int Coins { get; private set; }
-    
+
+    private void Awake()
+    {
+        coinsText.text = Coins.ToString();
+    }
     public void Add(int amount)
     {
         Coins += amount;
+        coinsText.text = Coins.ToString();
     }
 
     public int GetCoins()
