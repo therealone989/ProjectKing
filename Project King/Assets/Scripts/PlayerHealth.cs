0a09f2517e7508aebf64a6adf0a417de812a0527
therealone989 <mrfabigm@gmail.com>
2026-02-16 09:47:01 +0100

    PlayerHealth Responsive Damage

--- Project King/Assets/Scripts/PlayerHealth.cs
+++ Project King/Assets/Scripts/PlayerHealth.cs
@@ -4,19 +4,13 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-
     public Image[] healthBars;
+    float health;
+    float maxHealth = 100f;
 
-    float health, maxHealth = 100f;
-    float lerpSpeed;
     void Start()
     {
         health = maxHealth;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         HealthBarFiller();
     }
 
@@ -24,28 +18,32 @@ public class PlayerHealth : MonoBehaviour
     {
         if (context.performed)
         {
-            PlayerDamage(10);
+            PlayerDamage(7);
         }
-    }
-
-    bool DisplayHealthBars(float _health, int pointNumber)
-    {
-        return ((pointNumber * 11) >= _health);
     }
 
     void HealthBarFiller()
     {
+        // Wir berechnen den Wert pro Balken dynamisch basierend auf der Array-Größe
+        float healthPerBar = maxHealth / healthBars.Length;
+
         for (int i = 0; i < healthBars.Length; i++)
         {
-            healthBars[i].enabled = !DisplayHealthBars(health, i);
+            // Wir berechnen den Startpunkt dieses spezifischen Balkens (z.B. Bar 0 startet bei 0, Bar 1 bei 10...)
+            float barThreshold = i * healthPerBar;
+
+            // Wie viel Leben "gehört" in diesen Balken?
+            // Wir nehmen das aktuelle Leben, ziehen den Startpunkt ab und teilen durch die Kapazität des Balkens
+            float fillValue = (health - barThreshold) / healthPerBar;
+
+            // Der Wert muss zwischen 0 (leer) und 1 (voll) liegen
+            healthBars[i].fillAmount = Mathf.Clamp01(fillValue);
         }
     }
     public void PlayerDamage(float damagePoints)
     {
-        if(health > 0)
-        {
-            Debug.Log(health);
-            health -= damagePoints;
-        }
+        health -= damagePoints;
+        health = Mathf.Clamp(health, 0, maxHealth); // Verhindert Werte unter 0 oder über Max
+        HealthBarFiller();
     }
 }
