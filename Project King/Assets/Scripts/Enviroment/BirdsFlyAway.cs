560d86f9d3a67836d8b4e90bed47d7dc8daf258a
MarkiBoy69 <markriemann.mr@gmail.com>
2026-02-21 00:14:08 +0100

    Vögel fliegen + Archer

---
+++ Project King/Assets/Scripts/Enviroment/BirdsFlyAway.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class TriggerFly : MonoBehaviour
+{
+    public float flySpeed = 2f;   
+    public float forwardSpeed = 1f;  
+
+    private bool shouldFly;
+    private Animator[] animators;
+
+    void Awake()
+    {
+        animators = GetComponentsInChildren<Animator>(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        shouldFly = true;
+
+        foreach (var a in animators)
+            a.SetTrigger("Fly");
+    }
+
+    void Update()
+    {
+        if (!shouldFly) return;
+
+        float dt = Time.deltaTime;
+
+        foreach (var a in animators)
+        {
+            Transform t = a.transform;
+
+            Vector3 move = Vector3.up * flySpeed * dt
+                         + t.forward * forwardSpeed * dt;
+
+            t.position += move;
+        }
+    }
+}
